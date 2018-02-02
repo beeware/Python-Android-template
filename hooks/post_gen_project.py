77f0a164031388559add7205e3039c997c2f8fac
jvmaia <joaovmferreira@gmail.com>
2018-02-02 16:54:55 -0200

    exclude comments

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,24 +2,6 @@ from shutil import copyfile
 import glob
 import os
 
-"""
-base_path = os.path.split(os.getcwd())[0]
-base_path_libs = os.path.join(base_path, 'libs')
-target_path_libs = os.path.join(os.getcwd(), 'libs')
-
-try:
-    alldir = os.listdir(base_path_libs)
-    libs = filter(lambda x: x.endswith('.jar'), alldir)
-except:
-    libs = []
-
-for lib in libs:
-    base_path_lib = os.path.join(base_path_libs, lib)
-    target_path_lib = os.path.join(target_path_libs, lib)
-    copyfile(base_path_lib, target_path_lib)
-    print(' * Lib {} copied to android/libs/'.format(lib))
-    """
-
 for src in glob.glob('../libs/*.jar'):
     dest = os.path.join('libs', os.path.basename(src))
     copyfile(src, dest)
