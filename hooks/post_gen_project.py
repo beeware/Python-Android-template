c992fd0c555e1e7f5ae546507c1690520a61fcc4
jvmaia <joaovmferreira@gmail.com>
2018-02-04 17:27:19 -0200

    add support to custom Android Manifest

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,7 @@
 from shutil import copyfile
 import glob
 import os
+import jinja2
 
 for src in glob.glob('../libs/*.jar'):
     dest = os.path.join('libs', os.path.basename(src))
@@ -8,3 +9,13 @@ for src in glob.glob('../libs/*.jar'):
 
 print(' * All libs are already copied')
 
+base_path = os.path.split(os.getcwd())[0]
+manifest = 'AndroidManifest.xml'
+manifest_path = os.path.join(base_path, manifest)
+if os.path.isfile(manifest_path):
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(base_path))
+    content = {}
+    content['cookiecutter'] = {{ cookiecutter }}
+    output_manifest = env.get_template(manifest).render(content)
+    with open(manifest, 'w') as f:
+        f.write(output_manifest)
